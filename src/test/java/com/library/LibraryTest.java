b4ac07ad0ad87735146874e639b1a8baa556c06b
BottleUser222 <marko.s.mirkovic@gmail.com>
2025-10-12 13:32:56 +0200

    Test

--- src/test/java/com/library/LibraryTest.java
+++ src/test/java/com/library/LibraryTest.java
@@ -11,15 +11,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class LibraryTest {
 
     /*@Test
-   void testSearchBook(){
+    void testSearchBook(){
         ArrayList<Book> testLibrary = new ArrayList<>();
         Library library = new Library();
         Scanner sc = new Scanner("title\nMoby Dick");
         Book book = new Book("Moby Dick","Herman Melville","1851","123456","Available");
         testLibrary.add(book);
         assertEquals(book, library.searchBook(sc));
-    }*/
-    /*@Test
+    }
+    @Test
     void testBorrowBook(){
         ArrayList<Book> testLibrary = new ArrayList<>();
         Library library = new Library();
